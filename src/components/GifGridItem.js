9be980458aad8cd733e1400ee0df163da67fb73c
ciosmo <ig.navarroa@duocuc.cl>
2021-12-25 19:12:16 -0300

    pequenios cambios

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const GifGridItem = ( {id,title,url} ) => {
-    console.log(id,title,url);
+    
     return (
         <div className='card animate__fadeIn'>
             <img src={url} alt={title} />
